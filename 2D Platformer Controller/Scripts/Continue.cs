71957c502906329d6a363868f028ffd4210762d7
Stacey-Eva Fabrè <59258519+NondescriptMidnight@users.noreply.github.com>
2019-12-27 00:54:42 +0200

    Prefabs, materials and platform controller.

---
+++ 2D Platformer Controller/Scripts/Continue.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Continue : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	void OnMouseDown() {
+
+
+		Application.OpenURL("http://www.dddragon.cn/endgame.php"); 
+		//Application.Quit();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		if (Input.touchCount != 0) {
+			Application.OpenURL("http://www.dddragon.cn/endgame.php");
+
+			//Application.Quit();
+		}
+	
+	} 
+
+
+	void OnMouseOver() {
+
+
+		//Application.OpenURL("http://www.dddragon.cn/endgame.php"); 
+	//Application.Quit();
+
+	}
+}
